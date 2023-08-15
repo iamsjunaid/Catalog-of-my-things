dec10eab3551d2c2001b04d8440ce9a6cf14beae
Junaid Syed <sjunaid626@gmail.com>
2023-08-15 15:14:13 +0530

    create contructor

--- classes/item.rb
+++ classes/item.rb
@@ -1,2 +1,9 @@
+require 'date'
+
 class Item
+  def initialize(published_date, id, archived: false)
+    @id = id.nil? || random.rand(1...1000)
+    @published_date = Date.parse(published_date)
+    @archived = archived
+  end
 end
