c34e823e72948f767e595b4b273dd02a04c0aa28
DebyGrey <demifashoro@gmail.com>
2023-08-15 11:09:44 +0100

    Creates item methods

--- classes/item.rb
+++ classes/item.rb
@@ -2,8 +2,22 @@ require 'date'
 
 class Item
   def initialize(published_date, id, archived: false)
-    @id = id.nil? || random.rand(1...1000)
+    @id = id.nil? ? Random.rand(1...1000) : id
     @published_date = Date.parse(published_date)
     @archived = archived
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    current_date = Date.today
+    year = current_date.year - @published_date.year
+    year > 10
   end
 end
