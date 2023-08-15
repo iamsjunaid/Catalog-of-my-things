8af7292df97bd078b876c1d3e7c1a33e52fd41f1
Luis Zubia <soyzubia@gmail.com>
2023-08-15 12:40:12 -0700

    Add 'to_s' private method to display a 'MusicAlbum' correctly

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -13,4 +13,19 @@ class MusicAlbum < Item
   def can_be_archived?
     super && @on_spotify
   end
+
+  def to_s
+    properties = [
+      "id: #{@id}",
+      "publish_date: #{@publish_date}",
+      "on_spotify: #{@on_spotify ? 'yes' : 'no'}",
+      "archived?: #{@archived ? 'yes' : 'no'}"
+    ]
+
+    properties.push("genre: #{@genre.name}") unless @genre.nil?
+    properties.push("author first_name: #{@author.first_name} last_name: #{@author.last_name}") unless @author.nil?
+    properties.push("label title: #{@label.title} color: #{@label.color}") unless @label.nil?
+
+    properties.join(' | ')
+  end
 end
