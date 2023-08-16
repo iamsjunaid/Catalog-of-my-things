8f3e5ec4fb2acbc62b51e086a13c920570f5c9ca
Junaid Syed <sjunaid626@gmail.com>
2023-08-16 10:14:26 +0530

    add super to call the items class constructor

--- classes/Book/book.rb
+++ classes/Book/book.rb
@@ -4,11 +4,12 @@ class Book < Item
   attr_accessor :cover_state, :publisher
 
   def initialize(params)
+    super(params)
     @id = params[:id] || Random.rand(1..1000)
     @cover_state = params[:cover_state] || 'good'
     @publisher = params[:publisher] || 'Unknown'
   end
-
+  
   def can_be_archived
     super || (@cover_state == 'bad')
   end
