ba837ba4e3144b3e1d8ebdd36c86992f79b0feeb
Junaid Syed <sjunaid626@gmail.com>
2023-08-16 10:17:42 +0530

    fix linter errors

--- classes/Book/book.rb
+++ classes/Book/book.rb
@@ -9,7 +9,7 @@ class Book < Item
     @cover_state = params[:cover_state] || 'good'
     @publisher = params[:publisher] || 'Unknown'
   end
-  
+
   def can_be_archived
     super || (@cover_state == 'bad')
   end
