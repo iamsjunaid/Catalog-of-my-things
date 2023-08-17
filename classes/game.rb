33bddc1afa1fb89b7be030d1b6ba9f27460cbc31
DebyGrey <demifashoro@gmail.com>
2023-08-17 09:35:16 +0100

    Creates game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,17 +1,5 @@
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
-
-  def initialize(params)
-    super(params)
-    @multiplayer = params[:multiplayer]
-    @last_played_at = Date.parse(params[:last_played_at])
-  end
-
-  private
-
-  def can_be_archived?
-    super && @multiplayer && @last_played_at
-  end
+  
 end
